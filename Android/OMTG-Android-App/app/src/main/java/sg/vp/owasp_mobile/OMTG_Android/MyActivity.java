141f02d08f163375879629faf2b6a3235a14f678
Sven Schleier <sven@vantagepoint.sg>
2016-07-21 13:58:42 +0800

    first beta of Android App

---
+++ Android/OMTG-Android-App/app/src/main/java/sg/vp/owasp_mobile/OMTG_Android/MyActivity.java
@@ -0,0 +1,144 @@
+package sg.vp.owasp_mobile.OMTG_Android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MyActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+//    public void sendMessage(View view) {
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.edit_message);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//    }
+
+
+    public void OMTG_ENV_005_Webview_Remote(View view) {
+        Intent intent = new Intent(this, OMTG_ENV_005_WebView_Remote.class);
+        startActivity(intent);
+    }
+
+
+    public void OMTG_ENV_005_Webview_Local(View view) {
+        Intent intent = new Intent(this, OMTG_ENV_005_WebView_Local.class);
+        startActivity(intent);
+    }
+
+    public void OMTG_CODING_003_Best_Practice(View view) {
+        Intent intent = new Intent(this, OMTG_CODING_003_Best_Practice.class);
+        startActivity(intent);
+    }
+
+    public void OMTG_CODING_003_SQL_Injection(View view) {
+        Intent intent = new Intent(this, OMTG_CODING_003_SQL_Injection.class);
+        startActivity(intent);
+    }
+
+    public void OMTG_CODING_003_SQL_Injection_Content_Provider(View view) {
+        Intent intent = new Intent(this, OMTG_CODING_003_SQL_Injection_Content_Provider.class);
+        startActivity(intent);
+    }
+
+    public void OMTG_CODING_004_Code_Injection(View view) {
+        Intent intent = new Intent(this, OMTG_CODING_004_Code_Injection.class);
+        startActivity(intent);
+    }
+
+    public void OMTG_DATAST_001_BadEncryption(View view) {
+        Intent intent = new Intent(this, OMTG_DATAST_001_BadEncryption.class);
+        startActivity(intent);
+    }
+
+    public void OMTG_DATAST_001_KeyChain(View view) {
+        Intent intent = new Intent(this, OMTG_DATAST_001_KeyChain.class);
+        startActivity(intent);
+    }
+
+    public void OMTG_DATAST_001_KeyStore(View view) {
+        Intent intent = new Intent(this, OMTG_DATAST_001_KeyStore.class);
+        startActivity(intent);
+    }
+
+    public void OMTG_DATAST_002_Logging(View view) {
+        Intent intent = new Intent(this, OMTG_DATAST_002_Logging.class);
+        startActivity(intent);
+    }
+
+    public void OMTG_DATAST_004_SharedPreferences(View view) {
+        Intent intent = new Intent(this, OMTG_DATAST_004_SharedPreferences.class);
+        startActivity(intent);
+    }
+
+    public void OMTG_DATAST_004_InternalStorage(View view) {
+        Intent intent = new Intent(this, OMTG_DATAST_004_InternalStorage.class);
+        startActivity(intent);
+    }
+
+    public void OMTG_DATAST_004_ExternalStorage(View view) {
+        Intent intent = new Intent(this, OMTG_DATAST_004_ExternalStorage.class);
+        startActivity(intent);
+    }
+
+
+    public void OMTG_DATAST_004_SQLite_Not_Encrypted(View view) {
+        Intent intent = new Intent(this, OMTG_DATAST_004_SQLite_Not_Encrypted.class);
+        startActivity(intent);
+    }
+
+    public void OMTG_DATAST_004_SQLite_Encrypted(View view) {
+        Intent intent = new Intent(this, OMTG_DATAST_004_SQLite_Encrypted.class);
+        startActivity(intent);
+    }
+
+    public void OMTG_DATAST_005_3rd_Party(View view) {
+        Intent intent = new Intent(this, OMTG_DATAST_005_3rd_Party.class);
+        startActivity(intent);
+    }
+
+    public void OMTG_DATAST_007_Memory(View view) {
+        Intent intent = new Intent(this, OMTG_DATAST_007_Memory.class);
+        startActivity(intent);
+    }
+}
--- README.md
+++ README.md
@@ -1,9 +1,36 @@
 # OMTG-Hacking-Playground
 
-This app contains different test cases that map to the OWASP Mobile Testing Guide. Goal is to give developers/testers/security researchers an easy way to try out the different ways to analyse an Android App for security weaknesses and bad practices.
+## Project Description
+
+The OWASP Mobile Testing Guide (OMTG) will be similar to the OWASP Web Application Testing Guide, but will have speci c test cases only applicable to mobile platforms. In order to give practical guidance to developers, security researches and penetration testers, a hacking playground was created that consists of different mobile App’s that contain different vulnerabilities that map to the OMTG test cases. Every test case described in the OMTG will therefore be implemented in an Android and iOS App. This has two advantages:
+- A developer can identify vulnerable code in the provided App’s and can see the implications and risks if such patterns are used and can look for the best practices in the OMTG to mitigate the vulnerabilities.
+- Penetration testers / security researchers can identify bad practices, dangerous methods and classes they should look  rst when assessing a Mobile App and can gain more knowledge through the information provided in the OMTG.
+
 
 Current Draft of the OWASP Mobile Testing Guide (OMTG) can be found here: https://docs.google.com/document/d/132Ose0jdQwN6Z_Fp0VOJtVdGCufIwligwmf6oT0lmK8/edit#
  
 
 If you do not have access to the OMTG yet, please write a mail to sven@vantagepoint.sg. 
+
+
+## Mobile Apps
+
+The following Mobile App is already created:
+* A fully working Android Application is implemented with around 20 test cases, and will be extended incrementally during the work on the OMTG.
+For the future the following enhancements are planned:
+
+
+## Project Roadmap 
+
+### Short Term (1-3 months)
+* Include more test cases into the Android App so all possible test cases are included
+* Review the test cases
+* A branch of the Android App will be created with Code Obfuscation, by using ProGuard to show developers how to enable code obfuscation and how it looks like once it's activated but it's also a playground to show penetration testers/security researches ways to reverse engineer the code.
+* Keep the documentation up-to-date and the link to the OMTG
+* Presentation of the OWASP Mobile Testing Guide (status update) and Hacking Playground at Null Security in August 2016 at the Singapore Meetup.
+
+### Long Term (3-12 months)
+* Create an iOS App (including one with code obfuscation) that also includes the different test cases of the OWASP Mobile Testing Guide
+* Maintain the Android / iOS App in case the OWASP Mobile Testing Guide is getting updated, e.g. new best practices are available or new vulnerabilities are disclosed
+    
 
